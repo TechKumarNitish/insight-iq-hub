9b98cf71c8c81afb7ffbd2f55df3f6d3592dbe79
Nitish Kumar <rskban15a2015@gmail.com>
2025-07-28 15:37:21 +0000

    fixed bug to show the tag of tech in tech table

--- pages/Technology/jsobjects/ManageTech/ManageTech.js
+++ pages/Technology/jsobjects/ManageTech/ManageTech.js
@@ -5,7 +5,7 @@ export default {
 			const response=await get_technology.run();
 			const tagData=await get_tag.data
 			const techData=response.map(x=>{
-				const tags=x.tag?.map(y=>tagData.find(z=>z._id===y).tag_name).join(", ")
+				const tags=x.tags?.map(y=>tagData.find(z=>z._id===y).tag_name).join(", ")
 				return{
 					...x,
 					tags
